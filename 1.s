15d9298fb2bdd91ea68151cd80b509aca7be5564
alexs <34484473+AlekseySav@users.noreply.github.com>
2019-04-30 20:04:31 +0300

    added example of rasm file

---
+++ 1.s
@@ -0,0 +1,19 @@
+| stupid file
+
+.ascii "hello\n"		| ascii str
+
+.byte 0x10				| byte
+.word 0x10				| word
+.dword 0x10				| dword
+
+.byte 100b				| from bin
+.byte 100				| from dec
+.byte 0107				| from oct
+.byte 0xaa				| from hex
+
+.word 0, 2, 4, 0x10		| several args
+
+|
+| thst is...
+| all for now :-(
+|
